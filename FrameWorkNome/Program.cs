b9ef3e3cb9ed629838b305397e99df118d9627e4
Usuario <Usuario@CPD03-M10>
2024-02-27 11:04:31 -0300

    Adicionar arquivos de projeto.

---
+++ FrameWorkNome/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorkNome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("O nome digite foi: " + nome);
+
+            Console.ReadKey();
+        }
+    }
+}
